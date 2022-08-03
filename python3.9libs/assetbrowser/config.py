dd8ff1b30231e82f3d93451e755c382ca4733299
Foggyhazel <foggyhazel@outlook.com>
2022-08-03 23:35:37 +0700

    load setting

--- python3.9libs/assetbrowser/config.py
+++ python3.9libs/assetbrowser/config.py
@@ -1,8 +1,46 @@
+from genericpath import isfile
+import json
+import logging
 import os
 
-root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Assets')
+_setting_path = os.path.join(os.path.expanduser('~'), 'fh-asset')
+_default_settings = {
+    'root_path': os.path.join(_setting_path, 'Assets'),
+    'asset_label_color': (160, 160, 160, 255),
+    'init_panel_size': [240, 400, 200]
+}
+
+
+def _loadSettings(setting_dir: str):
+    settings_file = os.path.join(setting_dir, 'settings.json')
+    settings = {}
+
+    if os.path.isfile(settings_file):
+        with open(settings_file, 'r') as f:
+            try:
+                settings = json.load(f)
+            except Exception as e:
+                logging.warning('Cannot read setting file', e)
+    else:
+        # write default setting
+        os.makedirs(setting_dir, exist_ok=True)
+        with open(settings_file, 'w+') as f:
+            json.dump(_default_settings, f, indent=2)
+
+    settings = {k: v for (k, v) in settings.items() if v is not None}
+    return {**_default_settings, **settings}
+
+
 asset_file = 'asset.json'
 asset_def_file = 'assetDef.json'
 thumbnail_filename = 'thumbnail'
-asset_label_color = (160, 160, 160, 255)
-init_panel_sizes = [240, 400, 240]
+
+_settings = _loadSettings(_setting_path)
+
+root_path = _settings['root_path']
+asset_label_color = tuple(_settings['asset_label_color'])
+init_panel_sizes = _settings['init_panel_size']
+
+# ensure root path exist
+if not os.path.isdir(root_path):
+    os.makedirs(root_path)
